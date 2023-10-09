5bc1969215b0daade06ab6695cd0b494119a1de8
David Li <davidli012345@gmail.com>
2023-10-08 18:23:12 -0700

    feat: update contents

--- back-end/index.js
+++ back-end/index.js
@@ -4,9 +4,15 @@ const apiController = require('./controllers/api');
 
 const app = express();
 
-var corsOptions = {
-  origin: 'http://localhost:3000',
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+const whitelist = ['http://localhost:3000', 'http://127.0.0.1:3000']
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
 }
 
 app.use(express.json());
